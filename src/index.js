b1fd3cc01ce4217b36eb51bcf4c3f97a9ff54022
Roman Zolotarev <hi@romanzolotarev.com>
2019-04-11 05:26:51 +0000

    fetch and render jokes
    add fetchApi() and call it from useEffect() hook, use useState to store
    an array of jokes.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('ok')
+import { render } from 'react-dom'
+import React, { useEffect, useState } from 'react'
+
+const fetchApi = async (url, cb) => {
+  const response = await fetch(url)
+  const json = await response.json()
+  if (json.value === undefined) return
+  cb(json.value)
+}
+
+const App = ({ url }) => {
+  const [jokes, setJokes] = useState([])
+  useEffect(() => {
+    fetchApi(url, setJokes)
+  }, [url])
+  return <pre>{JSON.stringify(jokes, {}, 2)}</pre>
+}
+
+render(
+  <App url="https://api.icndb.com/jokes/random/10" />,
+  document.getElementById('root')
+)
